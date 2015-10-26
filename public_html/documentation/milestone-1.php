be800693c7e911c3ebaceab71bc0a38268c9a6a3
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-25 18:57:05 -0600

    added milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Personal Web Page</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Personal Web Page for Derek Mauldin</h1>
+		</header>
+
+		<main>
+
+			<h2>Purpose</h2>
+			<p>The purpose of this site is to help me gain a position as a web developer.</p>
+			<br />
+
+			<h2>Audience</h2>
+			<p>
+				The audience for this site is, primarily, potential employers. Secondly for peers and friends to see what
+				I'm up to.
+			</p>
+			<br />
+
+			<h2>Goals For This Site</h2>
+			<ul>
+				<li>To acquire full time employment</li>
+				<li>To server as a portfolio to showcase my work</li>
+			</ul>
+
+		</main>
+	</body>
+</html>
