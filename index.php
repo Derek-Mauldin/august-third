ec13f3955b48516367218436211694bc43ff151a
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-25 19:02:02 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8" />
+		<title>AugustThird</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>AugustThird</h1>
+		</header>
+	</body>
+</html>
