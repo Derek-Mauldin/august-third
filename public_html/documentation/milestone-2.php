041dbc08367c802f495c952ce88ccdd7c48796fe
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-11 19:54:54 -0700

    added milestone-2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Milestone2</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Milestone 2 for PWP</h1>
+		</header>
+
+		<h2>Persona</h2>
+		<p><b>Name</b>: Stacie Smith</p>
+		<p><b>Age:</b>  42</p>
+
+		<p>
+			<b>Profession:</b>  HR Manager<br>Stacie works for a Midsized company, FooBar Inc., looking to fill  a web developer positions.
+			The company is expanding it's web presence with online shoppiing.
+		</p>
+
+		<p>
+			<b>Technology:</b>  Stacie primarily uses her company provided, Windows Lenovo laptop.  It runs on Windows 7.
+			she also has an apple iphone 6.
+		</p>
+
+		<p>
+			<b>Attitudes and Behaviors:</b>  Stacie is married with two kids.  Is very busy at work and at home.  She will
+			spend 5 to 7 minutes looking at a candidates website as well as looking at the resume.  She will also look at Linkedin.
+		</p>
+
+		<p>
+			<b>Needs:</b>  To find experienced and junior web developers.  They must be personable, coachable, and a good fit
+			with the company culture.
+		</p>
+
+		<p>
+			<b>Goals:</b>  Stacie needs to be able to decide form the candidates website, resume, and Linked in profile if he/she
+			is someone she would like to interview.
+		</p>
+
+
+		<h2>Use Case</h2>
+		<p>
+			Stacie has received a resume and cover letter from me about the web development positions.  She is going to look at my
+			website and Linked in profile.
+		</p>
+
+		<p>
+			Stacie is using her Lenovo laptop from which she will view my website along with other candidates sites.  She will:
+			<ul>
+				<li>Go to my Home page and navigate to the about me page. She goes over the core skills in the side panel.</li>
+				<li>
+					From the about me page she will navigate to the portfolio from the menu in the header.  She will look at
+					the projects featured on the portfolio page.
+				</li>
+				<li>Stacie has decided that I am a good candidate and will setup an interview.</li>
+			</ul>
+		</p>
+
+		<h2>Site Map</h2>
+		<p>
+			Each page will have my contact information in the footer.
+			<ul>
+				<li>Home Page</li>
+				<li>Portfolio.  Each project will also include links to my GitHub Repository for the project.</li>
+				<li>About Me/Resume</li>
+			</ul>
+		</p>
+
+		<p>
+			<b>Home Page: </b> A brief introduction with my name and role as a Full Stack Developer,
+			and will feature 2 calls to action that will direct the user to the About Me or the Portfolio page.
+		</p>
+
+		<p>
+			<b>About Me:  </b>This page will contain my professional history, core skill set, and professional highlights.
+			This page will showcase my strengths and personality. I will feature a contrasting sidebar that lists my core
+			skills which will be easily readable at a glance. It will be personable and casual, as I am
+			only interested in opportunities with individuals and organizations that I am a <em>good personality</em> fit for.
+		</p>
+
+		<p>
+			<b>Protfolio:  </b>Examples of my work, including contributions to each project.
+		</p>
+
+	</body>
