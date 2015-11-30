0ed5dc4a4469e4cf7b2a75032e65117052f35084
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-29 17:24:38 -0700

    added milestone 3

---
+++ public_html/documentation/milestone-3/milestone-3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Personal Web Page</title>
+	</head>
+
+	<body>
+
+		<p>The main idea for my layout is that I will have a main content area that will change depending on the link that has been activated.</p>
+
+		<h1>Laptop Wirefram</h1>
+		<img src="Laptop%20Wireframe.png">
+
+		<h2>Mobile Wireframe</h2>
+		<img src="Mobile%20Wirefram.png">
+
+	</body>
+</html>
