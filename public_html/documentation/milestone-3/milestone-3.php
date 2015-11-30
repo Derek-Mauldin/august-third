e07fde22db04c6bafa075542c30d6dcb9fa1d1af
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-29 17:42:08 -0700

    corrected some things for  milestone 3

--- public_html/documentation/milestone-3/milestone-3.php
+++ public_html/documentation/milestone-3/milestone-3.php
@@ -9,7 +9,7 @@
 
 		<p>The main idea for my layout is that I will have a main content area that will change depending on the link that has been activated.</p>
 
-		<h1>Laptop Wirefram</h1>
+		<h1>Laptop Wireframe</h1>
 		<img src="Laptop%20Wireframe.png">
 
 		<h2>Mobile Wireframe</h2>
